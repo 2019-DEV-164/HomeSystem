2f635b048aaa5978199bf70e20999b651ea5620a
sam <sam.yohind@gmail.com>
2021-02-28 22:35:57 +0800

    Volume controls

---
+++ app/src/main/java/com/home/homesystem/viewModel/VolumeControlViewModel.kt
@@ -0,0 +1,19 @@
+package com.home.homesystem.viewModel
+
+import android.app.Activity
+import android.view.View
+import java.security.AccessControlContext
+
+class VolumeControlViewModel(var context: Activity) {
+    fun volUpPressed(view: View){
+
+    }
+
+    fun volDownPressed(view: View){
+
+    }
+
+    fun mutePressed(view: View){
+
+    }
+}
